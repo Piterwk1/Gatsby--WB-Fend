72f9d497a06337f4402b4e38809dbda5a712b1f1
piterwk1 <piterwk1@wp.pl>
2021-04-27 21:20:04 +0200

    add limit of 2 person per page display

--- gatsby-node.js
+++ gatsby-node.js
@@ -85,6 +85,40 @@ async function fetchBeersAndTurnIntoNodes({
   }
 }
 
+async function turnSlicemastersIntoPages({ graphql, actions }) {
+  const { data } = await graphql(`
+    query {
+      slicemasters: allSanityPerson {
+        totalCount
+        nodes {
+          name
+          id
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+
+  const pageSize = parseInt(process.env.GATSBY_PAGE_SIZE);
+  const pageCount = Math.ceil(data.slicemasters.totalCount / pageSize);
+  console.log(`${data.slicemasters.totalCount} ${pageCount} ${pageSize}`);
+
+  Array.from({ length: pageCount }).forEach((_, i) => {
+    console.log(`creating page ${i}`);
+    actions.createPage({
+      path: `/slicemasters/${i + 1}`,
+      component: path.resolve('./src/pages/slicemasters.js'),
+      context: {
+        skip: i * pageSize,
+        currentPage: i + 1,
+        pageSize,
+      },
+    });
+  });
+}
+
 exports.sourceNodes = async (params) => {
   await Promise.all([fetchBeersAndTurnIntoNodes(params)]);
 };
@@ -93,5 +127,6 @@ exports.createPages = async (params) => {
   await Promise.all([
     turnPizzasIntoPages(params),
     turnToppingsIntoPages(params),
+    turnSlicemastersIntoPages(params),
   ]);
 };
