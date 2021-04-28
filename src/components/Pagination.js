f98669840e4571b6f13d5c06eea5523215882c3a
piterwk1 <piterwk1@wp.pl>
2021-04-28 21:47:57 +0200

    add navigation for slicemasters page

---
+++ src/components/Pagination.js
@@ -0,0 +1,62 @@
+import { Link } from 'gatsby';
+import React from 'react';
+import styled from 'styled-components';
+
+const PaginationStyles = styled.div`
+  display: flex;
+  align-items: center;
+  align-content: center;
+  justify-items: center;
+  border: 1px solid var(--grey);
+  margin: 2rem 0;
+  border-radius: 5px;
+  text-align: center;
+  & > * {
+    padding: 1rem;
+    flex: 1;
+    border-right: 1px solid var(--grey);
+    text-decoration: none;
+    &[aria-current],
+    &.current {
+      color: var(--red);
+      /* background-color: var(--black); */
+    }
+    &[disabled] {
+      pointer-events: none;
+      color: var(--grey);
+    }
+  }
+`;
+
+export default function Pagination({
+  pageSize,
+  totalCount,
+  currentPage,
+  skip,
+  base,
+}) {
+  const totalPages = Math.ceil(totalCount / pageSize);
+  const prevPage = currentPage - 1;
+  const nextPage = currentPage + 1;
+  const hasNextPage = currentPage <= totalPages - 1;
+  const hasPrevPage = currentPage > 1;
+
+  return (
+    <PaginationStyles>
+      <Link disabled={!hasPrevPage} to={`${base}/${prevPage}`}>
+        🠔 Prev
+      </Link>
+      {Array.from({ length: totalPages }).map((_, i) => (
+        <Link
+          className={currentPage === 1 && i === 0 ? 'current' : ''}
+          to={`${base}/${i > 0 ? i + 1 : ''}`}
+        >
+          {i + 1}
+        </Link>
+      ))}
+      <Link disabled={!hasNextPage} to={`${base}/${nextPage}`}>
+        Next 🠖
+      </Link>
+    </PaginationStyles>
+  );
+}
--- src/pages/slicemasters.js
+++ src/pages/slicemasters.js
@@ -2,6 +2,7 @@ import { graphql, Link } from 'gatsby';
 import React from 'react';
 import Img from 'gatsby-image';
 import styled from 'styled-components';
+import Pagination from '../components/Pagination';
 
 const SlicemastersGrid = styled.div`
   display: grid;
@@ -36,11 +37,18 @@ const SicemasterStyles = styled.div`
   }
 `;
 
-export default function SlicemastersPage({ data }) {
+export default function SlicemastersPage({ data, pageContext }) {
   const slicemasters = data.slicemasters.nodes;
   console.log(slicemasters);
   return (
     <>
+      <Pagination
+        pageSize={parseInt(process.env.GATSBY_PAGE_SIZE)}
+        totalCount={data.slicemasters.totalCount}
+        currentPage={pageContext.currentPage || 1}
+        skip={pageContext.skip}
+        base="/slicemasters"
+      />
       <SlicemastersGrid>
         {slicemasters.map((person) => (
           <SicemasterStyles key={person.id}>
