49eaabebc2379de511d4c9580fb7ecbcef9d2a36
piterwk1 <piterwk1@wp.pl>
2021-04-29 20:11:06 +0200

    add page for single slicemaster

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -12,6 +12,7 @@ exports.components = {
   "component---src-pages-order-js": (preferDefault(require("C:\\MOJE\\JS\\WES BOSS\\GATSBY\\master-gatsby-master\\master-gatsby-master\\starter-files\\gatsby\\src\\pages\\order.js"))),
   "component---src-pages-pizzas-js": (preferDefault(require("C:\\MOJE\\JS\\WES BOSS\\GATSBY\\master-gatsby-master\\master-gatsby-master\\starter-files\\gatsby\\src\\pages\\pizzas.js"))),
   "component---src-pages-slicemasters-js": (preferDefault(require("C:\\MOJE\\JS\\WES BOSS\\GATSBY\\master-gatsby-master\\master-gatsby-master\\starter-files\\gatsby\\src\\pages\\slicemasters.js"))),
-  "component---src-templates-pizza-js": (preferDefault(require("C:\\MOJE\\JS\\WES BOSS\\GATSBY\\master-gatsby-master\\master-gatsby-master\\starter-files\\gatsby\\src\\templates\\Pizza.js")))
+  "component---src-templates-pizza-js": (preferDefault(require("C:\\MOJE\\JS\\WES BOSS\\GATSBY\\master-gatsby-master\\master-gatsby-master\\starter-files\\gatsby\\src\\templates\\Pizza.js"))),
+  "component---src-templates-slicemaster-js": (preferDefault(require("C:\\MOJE\\JS\\WES BOSS\\GATSBY\\master-gatsby-master\\master-gatsby-master\\starter-files\\gatsby\\src\\templates\\Slicemaster.js")))
 }
 
