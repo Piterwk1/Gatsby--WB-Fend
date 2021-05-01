6dda4edf2c154608aadd732232c332141a4887d6
piterwk1 <piterwk1@wp.pl>
2021-05-01 23:27:25 +0200

    add order component, formatMoney, calcPizzaPrice formHook

---
+++ src/utils/useForm.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+export default function useForm(defaults) {
+  const [values, setValues] = useState(defaults);
+
+  function updateValue(e) {
+    // check if its a number and convert
+    let { value } = e.target;
+    if (e.target.type === 'number') {
+      value = parseInt(e.target.value);
+    }
+    setValues({
+      // copy the existing values into it
+      ...values,
+      // update the new value that changed
+      [e.target.name]: value,
+    });
+  }
+
+  return { values, updateValue };
+}
+
+// import React, { useState } from 'react';
+
+// export default function useForm(defaults) {
+//   const [values, setValues] = useState(defaults);
+
+//   function updateValue(e) {
+//     let { value } = e.target;
+//     if (e.target.value === 'number') {
+//       //   parseInt(value);
+//       value = parseInt(e.target.value);
+//     }
+
+//     setValues({
+//       ...values,
+//       [e.target.name]: value,
+//     });
+//   }
+
+//   return { values, setValues };
+// }
