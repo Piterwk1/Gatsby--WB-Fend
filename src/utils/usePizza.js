653b2574f5cf1d7098b5ecf0293a6e164edb16ee
piterwk1 <piterwk1@wp.pl>
2021-05-06 20:56:46 +0200

    modifying hook to send order data

--- src/utils/usePizza.js
+++ src/utils/usePizza.js
@@ -1,12 +1,18 @@
 import { useContext, useState } from 'react';
 import OrderContext from '../components/OrderContext';
+import attachNamesAndPrices from './attachNamesAndPrices';
+import calculateOrderTotal from './calculateOrderTotal';
+import formatMoney from './formatMoney';
 
-export default function usePizza({ pizza, inputs }) {
+export default function usePizza({ pizzas, values }) {
   // moved useState up to provider
 
   //   const [order, setOrder] = useState([]);
   // acces state and setOrder via context
   const [order, setOrder] = useContext(OrderContext);
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState('');
 
   function addToOrder(orderPizza) {
     setOrder([...order, orderPizza]);
@@ -15,9 +21,48 @@ export default function usePizza({ pizza, inputs }) {
   function removeFromOrder(index) {
     setOrder([...order.slice(0, index), ...order.slice(index + 1)]);
   }
+
+  async function submitOrder(e) {
+    e.preventDefault();
+    console.log(e);
+    setLoading(true);
+    setError(null);
+    setMessage(null);
+    const body = {
+      order: attachNamesAndPrices(order, pizzas),
+      total: formatMoney(calculateOrderTotal(order, pizzas)),
+      name: values.name,
+      email: values.email,
+    };
+
+    const res = await fetch(
+      `${process.env.GATSBY_SERVERLESS_BASE}/placeOrder`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      }
+    );
+    const tex = JSON.parse(await res.text());
+
+    if (res.status >= 400 && res.status < 600) {
+      setLoading(false);
+      setError(text.message);
+    } else {
+      setLoading(false);
+      setMessage('Succes! Come on dawn for your Pizza');
+    }
+  }
+
   return {
     order,
     addToOrder,
     removeFromOrder,
+    error,
+    loading,
+    message,
+    submitOrder,
   };
 }
