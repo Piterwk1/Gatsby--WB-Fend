334f109213bf32b27fbbdfb95399519873587716
piterwk1 <piterwk1@wp.pl>
2021-05-21 07:39:16 +0200

    add @media for nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -40,12 +40,37 @@ const NavStyles = styled.nav`
   a {
     font-size: 3rem;
     text-decoration: none;
+    display: block;
     &:hover {
       color: var(--red);
+    }
+    @media (max-width: 800) {
+      font-size: 2rem;
     }
     /* &[aria-current='page'] {
       color: red;
     } */
+  }
+  @media (max-width: 600) {
+    --columns: 4;
+    margin-bottom: 2rem;
+    border-bottom: 2px solid var(--grey);
+    padding-bottom: 2rem;
+    ul {
+      grid-template-rows: auto auto;
+      grid-template-columns: repeat(var(--columns), 1fr);
+      justify-items: center;
+    }
+    .logo-item {
+      order: 0;
+      grid-column: 1 / -1;
+    }
+    .logo {
+      transform: none;
+    }
+  }
+  @media (max-width: 500) {
+    --columns: 2;
   }
 `;
 
@@ -60,7 +85,7 @@ export default function Nav() {
         <li>
           <Link to="/pizzas">Pizzas Menu</Link>
         </li>
-        <li>
+        <li className="logo-item">
           <Link to="/">
             <Logo />
           </Link>
